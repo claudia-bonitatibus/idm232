ba19a6d7c264e096842410a722a312e9bff62094
claudia-bonitatibus <32339275+claudia-bonitatibus@users.noreply.github.com>
2018-06-14 18:57:21 -0400

    Update results.php

--- results.php
+++ results.php
@@ -1,6 +1,8 @@
 <?php
 	require "includes/db.php";
+?>
 
+<?php
 	if (isset($_GET['keyword'])) {
 		$searchRecipes = $_GET['keyword'];
 		//FIND ALL CONTENTS THAT MATCH WHAT THE USER SEARCHED
@@ -9,18 +11,19 @@
 	}
 
 
-$recipe_ids = [];
-// LOOP THROUGH ALL RESULTS AND FIND RECIPE ID
-while ( $row = mysqli_fetch_assoc($results) ) { 
-	$recipe_ids[] = $row['recipe_id'];
-}
+	$recipe_ids = [];
+	// LOOP THROUGH ALL RESULTS AND FIND RECIPE ID
+	while ( $row = mysqli_fetch_assoc($results) ) { 
+		$recipe_ids[] = $row['recipe_id'];
+	}
 
-// CONVERT ARRAY TO STRING SEPERATED BY COMMAS
-$recipe_ids = join(',', $recipe_ids);
+	// CONVERT ARRAY TO STRING SEPERATED BY COMMAS
+	$recipe_ids = join(',', $recipe_ids);
 
-//GET RECIPE FROM THE DATABASE WHER THE ID MATCHES THE MAIN ID
-$recipes = mysqli_query($connection, "SELECT * FROM main WHERE recipe_id in ($recipe_ids) ");
+	//GET RECIPE FROM THE DATABASE WHER THE ID MATCHES THE MAIN ID
+	$recipes = mysqli_query($connection, "SELECT * FROM main WHERE recipe_id in ($recipe_ids) ");
 ?>
+<!-- POPULATE PAGE -->
 <?php
 	$pageTitle='Search';
 	require "includes/header.php";
@@ -63,23 +66,19 @@ $recipes = mysqli_query($connection, "SELECT * FROM main WHERE recipe_id in ($re
 			// Step 4: Release Returned Data
 			mysqli_free_result($result);
 
-		
-	} else {
-		?> 
-			<p>No results found for your search</p>
-			<p>There are no recipes in our database related to your search</p>		
+			} else {
+				?> 
+					<p>No results found for your search</p>
+					<p>There are no recipes in our database related to your search</p>	
 
+				<?php
+					}
+					// Step 5: Close Database Connection
+					mysqli_close($connection);
+				?>
+
+				</div>
+			</main>
 		<?php
- 
-	}
-
-
-			// Step 5: Close Database Connection
-			mysqli_close($connection);
-		?>
-
-		</div>
-	</main>
-<?php
 	require "includes/footer.php";
 ?>
